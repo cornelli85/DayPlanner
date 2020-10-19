d55abcb25125d7a1752f2c352abfd05ff63bd746
Caesar <61262789+cornelli85@users.noreply.github.com>
2020-10-18 23:25:42 -0400

    adding-jquery

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    // We write code here to display the current date and time to the element with id #current day
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    var currentHour = moment().format()
+
+})
