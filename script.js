8f643648699e4cfd3019b326a352db49986bd1af
Caesar <61262789+cornelli85@users.noreply.github.com>
2020-10-19 15:24:39 -0400

    added more functions to differentiate time block

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // We write code here to display the current date and time to the element with id #current day
     $("#currentDay").text(moment().format('MMMM Do YYYY, HH:mm:ss a'));
 
+    function compareHours() {
 
-    //We create a variable to store the current hour of the day.
-    var currentHour = moment().format('MMMM Do YYYY, HH:mm:ss a');
 
-    
+        //We create a variable to store the current hour of the day.
+        var currentHour = parseInt(moment().format('H'));
 
-    $(".row").each(function() {
 
-        var hourRow = $(this).child(".hour").text()
-    }) 
-    
+
+        $(".row").each(function () {
+
+            var hourRow = parseInt($(this).children(".hour").text());
+
+
+            
+            if (hourRow < currentHour) {
+                $(this).addClass("past");
+            }
+
+            else if (hourRow === currentHour) {
+                $(this).addClass("present");
+            }
+
+            else {
+                $(this).addClass("future");
+            }
+            
+        });
+
+    }
+
+    compareHours ();
+
     
 
 })
