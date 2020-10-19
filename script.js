1f11fbada3d830dd54d3e46874770b26e6d242d4
Caesar <61262789+cornelli85@users.noreply.github.com>
2020-10-19 18:19:17 -0400

    added functionality to the save buttons

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ $(document).ready(function () {
     // We write code here to display the current date and time to the element with id #current day
     $("#currentDay").text(moment().format('MMMM Do YYYY, HH:mm:ss a'));
 
+    //We create a function to compare the various time blocks with the current hour of the day.
     function compareHours() {
 
 
-        //We create a variable to store the current hour of the day.
+        //We start by making a variable to store the current hour of the day.
         var currentHour = parseInt(moment().format('H'));
 
 
 
+        // We loop through each time block using the .each method.
         $(".row").each(function () {
 
+
+            //Make a variable to store the time for each time block by using jQuery to get the text in the target div.
             var hourRow = parseInt($(this).children(".hour").text());
 
 
-            
+
+            //Create conditionals to assign classes to each time block by comparing the time of each time block to the current hour.
             if (hourRow < currentHour) {
                 $(this).addClass("past");
             }
@@ -28,13 +33,25 @@ $(document).ready(function () {
             else {
                 $(this).addClass("future");
             }
-            
+
         });
 
     }
 
-    compareHours ();
+    compareHours();
 
-    
+    $(".saveBtn").on("click", function() {
+        var event = $(this).siblings(".description").val();
+
+        var timeRow = $(this).parent().attr("id");
+
+        localStorage.setItem(timeRow, event);
+
+        
+    });
+
+    var display = localStorage.getItem(timeRow);
+
+    $(".description").val(display);
 
 })
